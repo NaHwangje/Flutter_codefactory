b14db1f9dded6fb36bebd9dc6688b9dc32829745
Hwangje <na03266@naver.com>
2023-12-18 08:56:30 +0900

    스프레드 스크린

---
+++ splash_screen/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(SplashScreen()); //이 위젯을 첫 화면으로 지정
+}
+
+class SplashScreen extends StatelessWidget {
+
+  //위젯의 UI 구현
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            color: Colors.orange,
+          ),
+          child: Center(
+            child: Text('Splash Screen'),
+          ),
+        ),
+      ),
+    );
+  }
+}
