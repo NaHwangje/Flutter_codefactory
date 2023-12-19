1e55721a58d0997af9e834301d2f554bb53daed9
Hwangje <na03266@naver.com>
2023-12-19 17:15:49 +0900

    Update random dice

---
+++ random_dice/lib/screen/root_screen.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class RootScreen extends StatefulWidget {
+  const RootScreen({Key? key}) : super(key: key);
+
+  @override
+  State<RootScreen> createState() => _RootScreenState();
+}
+
+class _RootScreenState extends State<RootScreen> with TickerProviderStateMixin {
+  TabController? controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = TabController(length: 2, vsync: this);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: TabBarView(
+        controller: controller,
+        //탭 화면을 보여줄 위젯
+        children: renderChildren(),
+      ),
+      // 아래 탭 내비게이션을 구현하는 매개변수
+      bottomNavigationBar: renderBottomNavigation(),
+    );
+  }
+
+  List<Widget> renderChildren() {
+    return [];
+  }
+
+  BottomNavigationBar renderBottomNavigation() {
+    //탭 네비게이션을 구현하는 위젯
+    return BottomNavigationBar(
+      items: [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.edgesensor_high_outlined),
+          label: '주사위',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(
+            Icons.settings,
+          ),
+          label: '설정',
+        ),
+      ],
+    );
+  }
+}
