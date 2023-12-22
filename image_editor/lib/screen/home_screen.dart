cd703bbbe0ee479acf112018adf6e80615b40781
Hwangje <na03266@naver.com>
2023-12-22 11:26:52 +0900

    update image_editor

---
+++ image_editor/lib/screen/home_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:image_editor/component/main_app_bar.dart';
+import 'package:image_picker/image_picker.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  XFile? image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            child: MainAppBar(
+              onPickImage: onPickImage,
+              onDeleteItem: onDeleteItem,
+              onSaveImage: onSaveImage,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  void onPickImage() async {
+    final image = await ImagePicker().pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      this.image = image;
+    });
+  }
+
+  void onDeleteItem() {}
+
+  void onSaveImage() {}
+}
