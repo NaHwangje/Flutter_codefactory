87700befa600e949d71c8896723524c88678e3cc
나황제 <na03266@livewalk.co.kr>
2024-01-17 11:15:32 +0900

    update

---
+++ flutter/calender_scheduler/android/app/src/main/kotlin/ai/hj/calendarscheduler/MainActivity.kt
@@ -0,0 +1,6 @@
+package ai.hj.calendarscheduler
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter/cf_tube/lib/const/api.dart
@@ -0,0 +1,3 @@
+const API_KEY = "AIzaSyB1V3vt5nibKwhmCD4zTCTu7HoaaluX-ss";
+const YOUTUBE_API_BASE_URL = 'https://youtube.googleapis.com/youtube/v3/search';
+const CF_CHANNEL_ID = 'UCxZ2AlaT0hOmxzZvbF_j_Sw';
