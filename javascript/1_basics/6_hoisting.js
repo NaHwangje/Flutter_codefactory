325531772349ecd3ef286632416667683d2f5a07
나황제 <na03266@livewalk.co.kr>
2024-01-22 20:14:49 +0900

    update

--- javascript/1_basics/6_hoisting.js
+++ javascript/1_basics/6_hoisting.js
@@ -4,7 +4,7 @@
  * 모든 변수의 선언문이 코드의 최상단으로 이동되는 것처럼 느껴지는 현상을 이야기한다.
  */
 
-console.log(name);
+// console.log(name);
 // let name = 'Fixer';
 
 
---
+++ javascript/1_basics/7_operators.js
@@ -0,0 +1,17 @@
+//NaN => not a Number
+/**
+ * 할당 연산자 (assignment operator)
+ */
+number = 100;
+console.log(number);
+
+number += 10;
+console.log(number);
+
+/**
+ * 비교 연산자
+ * 1) 값의 비교
+ * 2) 값과 타입의 비교
+ */
+
+console.log(5 == 5);
