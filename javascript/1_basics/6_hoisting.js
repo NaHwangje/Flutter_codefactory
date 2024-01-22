e24f77ce4d6290b161c58613e1fe436ff1f69256
나황제 <na03266@livewalk.co.kr>
2024-01-22 19:48:21 +0900

    Update

---
+++ javascript/1_basics/6_hoisting.js
@@ -0,0 +1,20 @@
+/**
+ * Hoistiog
+ * 
+ * 모든 변수의 선언문이 코드의 최상단으로 이동되는 것처럼 느껴지는 현상을 이야기한다.
+ */
+
+console.log(name);
+// let name = 'Fixer';
+
+
+// console.log(name);
+
+// var name;
+// console.log(name);
+// name = "Fixer";
+// console.log(name);
+
+
+// console.log(yuJin);
+// let yuJin
