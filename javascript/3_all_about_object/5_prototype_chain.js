f3c4c8599bae45ceb3a618bcb2fc170e32cb0654
나황제 <na03266@livewalk.co.kr>
2024-01-30 10:42:43 +0900

    Update

---
+++ javascript/3_all_about_object/5_prototype_chain.js
@@ -0,0 +1,29 @@
+/**
+ * Prototype
+ */
+const testObj = {};
+
+//__proto__ 모든 객체에 존재하는 프로퍼티다.
+//class강의에서 배울때 상속에서 부모 클래스에 해당되는 값이다.
+console.log(testObj.__proto__);
+
+function IdolModel(name, year) {
+  this.name = name;
+  this.year = year;
+}
+
+console.log(IdolModel.prototype);
+
+// console.dir(IdolModel.prototype, {
+//   showHidden: true,
+// });
+
+//circular reference
+console.log(IdolModel.prototype.constructor === IdolModel);
+console.log(IdolModel.prototype.constructor.prototype === IdolModel.prototype);
+
+const yuJin = new IdolModel('안유진', 2003);
+
+console.log(yuJin.__proto__);
+console.log(yuJin.__proto__ === IdolModel.prototype);
+
